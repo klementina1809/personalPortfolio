c6ea04fd6c6bcf04432e6929a7e6436e7f1de086
klementina1809 <klementina.ua@gmail.com>
2024-03-15 18:50:19 +0100

    project setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { Row, Col, Container } from 'react-grid-system'
+
+function Header() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Projects.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { Row, Col, Container } from 'react-grid-system'
+
+
+function Projects() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Projects
