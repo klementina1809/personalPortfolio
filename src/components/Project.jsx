5a4fb9689eab539028e6e1bdd960004a77e10a10
Albina Khomenko <klementina.ua@gmail.com>
2026-03-14 21:28:39 +0100

    add new project coming soon

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,10 +5,16 @@ import ProjectModal from "./ProjectModal";
 function Project({ data }) {
 	const { t } = useTranslation();
 	const [modalOpen, setModalOpen] = useState(false);
-
 	return (
 		<div className="project">
-			{data.video ? (
+			{data.comingSoon ? (
+				<div className="coming-soon-wrapper">
+					<img src={data.img} alt="" />
+					<div className="coming-soon-overlay">
+						<span>Coming soon</span>
+					</div>
+				</div>
+			) : data.video ? (
 				<a href={data.linkWeb} target="_blank" rel="noreferrer" className="video-wrapper">
 					<video src={data.video} autoPlay muted playsInline loop />
 				</a>
@@ -23,22 +29,31 @@ function Project({ data }) {
 					<p className="description">{t(`projectDescriptions.${data.id}`)}</p>
 				</div>
 				<div className="button-container">
-					{data.linkWeb && data.linkWeb.trim() !== "" ? (
-						<a href={data.linkWeb} target="_blank" rel="noreferrer">
-							Demo
-						</a>
+					{data.comingSoon ? (
+						<>
+							<span className="disabled-button">Demo</span>
+							<span className="disabled-button">Read more</span>
+						</>
 					) : (
-						<span className="disabled-button">Demo</span>
-					)}
-					{!data.aiFree && (
-						<button className="demo-button" onClick={() => setModalOpen(true)}>
-							Read more
-						</button>
-					)}
-					{data.linkGit !== null && (
-						<a href={data.linkGit} target="_blank" rel="noreferrer">
-							GitHub
-						</a>
+						<>
+							{data.linkWeb && data.linkWeb.trim() !== "" ? (
+								<a href={data.linkWeb} target="_blank" rel="noreferrer">
+									Demo
+								</a>
+							) : (
+								<span className="disabled-button">Demo</span>
+							)}
+							{!data.aiFree && (
+								<button className="demo-button" onClick={() => setModalOpen(true)}>
+									Read more
+								</button>
+							)}
+							{data.linkGit !== null && (
+								<a href={data.linkGit} target="_blank" rel="noreferrer">
+									GitHub
+								</a>
+							)}
+						</>
 					)}
 				</div>
 			</div>
